7137fafdf0e60cdc5ab8afa869fd69874d6aea82
random-sir <antoni.torres@outlook.com>
2024-02-03 03:04:49 -0300

    Added fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,16 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    
+    #Pacman abbreviations
+    abbr pi "sudo pacman -S"
+    abbr pu "sudo pacman -Syu"
+    abbr pd "sudo pacman -Rs"
+    abbr pq "pacman -Q"
+    abbr pf "pacman -F /usr/bin/"
+
+    #Git abbreviations
+    abbr gco "git checkout"
+    abbr gcm "git checkout master"
+    abbr gca "git commit -a"
+    abbr gl "git log"
+end
