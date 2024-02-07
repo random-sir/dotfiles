dac0d77b7ecaab38d1595205f5315dfefd3bb29c
random-sir <antoni.torres@outlook.com>
2024-02-07 00:20:05 -0300

    Added gnome install to bootstrap
    Added optional gnome installation (plus pop shell) to bootstrap while
    also updating TODO to reflect the still missing work.

--- TODO.md
+++ TODO.md
@@ -1,5 +1,7 @@
 # TODO LIST
 
-- [ ] Add gnome install option
-- [ ] Add pop-shell and pop launcher install option
-- [X] Update README
+- [X] Add gnome install option
+- [X] Add pop-shell and pop launcher install option
+- [ ] Update README
+- [ ] Add nbfc configuration
+- [ ] Finish driver configuration 
--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ cd ~/dotfiles
 stow .
 
 #install usual packages
-sudo pacman -S --needed base-devel git neovim fish newsboat wezterm ttf-fira-code
+sudo pacman -S --needed --noconfirm base-devel git neovim fish newsboat wezterm ttf-fira-code
 
 #Make Build directory
 [ ! -d ~/Builds ] && mkdir ~/Builds
@@ -18,3 +18,24 @@ sudo pacman -S --needed base-devel git neovim fish newsboat wezterm ttf-fira-cod
 #Build Yay
 cd ~/Builds/
 [ ! -f /usr/bin/yay ] && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+
+#Install yay packages
+yay -S nbfc-linux
+
+#Optional gnome install and configuration
+read -p 'Do you wish to install and configure gnome? (Y/n): ' gnomeOption
+
+if [[ ! $gnomeOption =~ ^[Nn]$ ]]; then
+	#Installing gnome and xorg
+	sudo pacman -S --needed --noconfirm xorg gnome gnome-extra
+	#Installing nvidia drivers
+	sudo pacman -S --needed --noconfirm lib32-nvidia-utils nvidia nvidia-prime nvidia-utils
+	#Installing pop-shell things
+	sudo pacman -S --needed --noconfirm typescript make
+	cd ~/Builds
+	git clone https://github.com/pop-os/shell.git
+	cd shell
+	make local-install
+	gsettings --schemadir ~/.local/share/gnome-shell/extensions/pop-shell@system76.com/schemas set org.gnome.shell.extensions.pop-shell activate-launcher "['<Super>space']"
+	yay -S pop-launcher pop-theme
+fi
