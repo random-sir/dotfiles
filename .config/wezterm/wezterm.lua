0d1951955629399215770b5024efc14cae55e015
random-sir <antoni.torres@outlook.com>
2024-03-01 13:53:57 -0300

    Fixed slight issue with wezterm config
    I messed around with nvchad and it recommended this modification to my
    wezterm config.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,8 +5,6 @@ config.hyperlink_rules = wezterm.default_hyperlink_rules()
 local mux = wezterm.mux
 local act = wezterm.action
 
--- This table will hold the configuration.
-local config = {}
 
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
